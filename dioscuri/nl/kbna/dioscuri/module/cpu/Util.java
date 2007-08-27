99cbf767bc6cb6bf6909a9aa8bfdf181377305d9
bramfoo <bramfoo@users.noreply.github.com>
2007-08-27 07:48:00 +0000

    Removed unnecessary includes

--- dioscuri/nl/kbna/dioscuri/module/cpu/Util.java
+++ dioscuri/nl/kbna/dioscuri/module/cpu/Util.java
@@ -1,5 +1,5 @@
 /*
- * $Revision: 1.1 $ $Date: 2007-07-02 14:31:40 $ $Author: blohman $
+ * $Revision: 1.2 $ $Date: 2007-08-27 07:48:00 $ $Author: blohman $
  * 
  * Copyright (C) 2007  National Library of the Netherlands, Nationaal Archief of the Netherlands
  * 
@@ -33,8 +33,6 @@
  */
 
 package nl.kbna.dioscuri.module.cpu;
-
-import java.util.logging.Level;
 
 public class Util {
 	
