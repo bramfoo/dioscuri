36df001545bf7e3e2f738334e69fc84a3192c771
bkiers <bart@big-o.nl>
2010-09-24 14:56:41 +0000

    Small change to autoatically enable a certain hard disk (in the XML config file) when it is provided on the command line.

--- dioscuri/src/main/dioscuri/CommandLineInterface.java
+++ dioscuri/src/main/dioscuri/CommandLineInterface.java
@@ -123,10 +123,12 @@ public class CommandLineInterface {
             File hdImg = Utilities.resolvePathAsFile(commandLine.getOptionValue("d1"));
             logger.log(Level.INFO, " [cli] using custom first hard disk image: "+hdImg);
             if(hdImg == null || !hdImg.exists() || !hdImg.isFile()) {
+                emuConfig.getArchitecture().getModules().getAta().getHarddiskdrive().get(0).setEnabled(false);
                 throw new IOException(" [cli] hard disk image '"+hdImg.getName()+
                         "' does not exist in folder '"+hdImg.getParentFile().getAbsolutePath()+"'");
             }
             emuConfig.getArchitecture().getModules().getAta().getHarddiskdrive().get(0).setImagefilepath(hdImg.getAbsolutePath());
+            emuConfig.getArchitecture().getModules().getAta().getHarddiskdrive().get(0).setEnabled(true);
             changes = true;
         }
 
@@ -134,10 +136,12 @@ public class CommandLineInterface {
             File hdImg = Utilities.resolvePathAsFile(commandLine.getOptionValue("d2"));
             logger.log(Level.INFO, " [cli] using custom second hard disk image: "+hdImg);
             if(hdImg == null || !hdImg.exists() || !hdImg.isFile()) {
+                emuConfig.getArchitecture().getModules().getAta().getHarddiskdrive().get(0).setEnabled(false);
                 throw new IOException(" [cli] hard disk image '"+hdImg.getName()+
                         "' does not exist in folder '"+hdImg.getParentFile().getAbsolutePath()+"'");
             }
             emuConfig.getArchitecture().getModules().getAta().getHarddiskdrive().get(1).setImagefilepath(hdImg.getAbsolutePath());
+            emuConfig.getArchitecture().getModules().getAta().getHarddiskdrive().get(1).setEnabled(true);
             changes = true;
         }
 
